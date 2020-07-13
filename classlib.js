a4b30647c9042b43fa755496174b78c877fd29e3
Student <student@lan.itstep.kiev.ua>
2020-07-13 21:15:25 +0300

    END VALIDATION

--- classlib.js
+++ classlib.js
@@ -9,19 +9,22 @@ class Validator{
 	}
 
   checkLogin(login_value){
-    this.loginOk = true;
+  	let reg = /^[a-zA-Z][a-zA-Z0-9\-]{6,10}$/;
+    this.loginOk = reg.test(login_value);
  };
 
   checkPass1(pass1_value){
- 	this.pass1Ok = true;
+  	let reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_\.\-]{8,16}$/;
+ 	this.pass1Ok = reg.test(pass1_value);
  };
 
-  checkPass2(pass2_value){
- 	this.pass2Ok = true;
+  checkPass2(pass1_value, pass2_value){
+ 	this.pass2Ok = (pass1_value == pass2_value);
  };
 
   checkEmail(email_value){
- 	this.emailOk = true;
+  	let reg = /^[a-zA-Z0-9_\.\-]+@[a-z]+\.[a-z]{2,4}$/;
+ 	this.emailOk = reg.test(email_value);
  };
  validate(){
  	return (this.loginOk == true && this.pass1Ok == true && this.pass2Ok == true && this.emailOk == true);
@@ -30,28 +33,40 @@ class Validator{
 }
 
 class Controller{
-  constructor(loginID, pass1Id, pass2Id, emailId, form1Id){
+  constructor(loginID, pass1Id, pass2Id, emailId, form1Id, info1, info2, info3, info4){
   	this.login = document.querySelector(loginID);
   	this.pass1 = document.querySelector(pass1Id);
   	this.pass2 = document.querySelector(pass2Id);
   	this.email = document.querySelector(emailId);
   	this.form1 = document.querySelector(form1Id);
-
+    
   	this.validator = new Validator();
+
+  	this.login_info = document.querySelector(info1);
+  	this.pass1_info = document.querySelector(info2);
+  	this.pass2_info = document.querySelector(info3);
+  	this.email_info = document.querySelector(info4);
   }
   activateListeners(){
-  	this.login.addEventListener('blur', ()=>{  this.validator.checkLogin(this.login.value);  });
+  	this.login.addEventListener('blur', ()=>{ 
+  	 this.validator.checkLogin(this.login.value);
+  	 if(this.validator.loginOk){
+  	 	this.login.style.boxShadow = '0 0 green, 0 0 10px green';
+  	 	this.login_info.style = 'none';
+  	 } else {
+  	 	this.login.style.boxShadow = '0 0 red, 0 0 10px red';
+  	 	this.login_info.style = 'inline';
+  	 } 
+  	});
   	this.pass1.addEventListener('blur', ()=>{  this.validator.checkPass1(this.pass1.value);  });
-  	this.pass2.addEventListener('blur', ()=>{  this.validator.checkPass2(this.pass2.value);  });
-  	this.email.addEventListener('blur', ()=>{  this.validator.checkEmail(this.email.value); });
+  	this.pass2.addEventListener('blur', ()=>{  this.validator.checkPass2( this.pass1.value, this.pass2.value);  });
+  	this.email.addEventListener('blur', ()=>{  this.validator.checkEmail(this.email.value);  });
   	this.form1.addEventListener('submit', ()=>{  
-         alert(this.validator.validate());
          if (this.validator.validate()) {
          	this.form1.submit();
          } else {
          	alert('Отправка данных заблокирована!');
          }
-         
   	  });
   }
 }
--- reg.js
+++ reg.js
@@ -1,6 +1,6 @@
 'use strict';
 
 window.addEventListener('load', ()=>{
-  const controller = new Controller('#login', '#pass1', '#pass2', '#email', '#form1');
+  const controller = new Controller('#login', '#pass1', '#pass2', '#email', '#form1', '#login_info', '#pass1_info', '#pass2_info', '#email_info');
   controller.activateListeners();
 });
